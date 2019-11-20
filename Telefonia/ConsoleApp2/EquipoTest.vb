2746a626f35d29efac5b6f13e9e11bf10cc9e39c
enzo ortigoza <kill.enzo.caicer@gmail.com>
2019-11-20 19:08:01 -0300

    EquipoTest

--- Telefonia/ConsoleApp2/EquipoTest.vb
+++ Telefonia/ConsoleApp2/EquipoTest.vb
@@ -1,3 +1,16 @@
-﻿Module EquipoTest
+﻿Imports TelefoniaEntidades
+Module EquipoTest
+    Sub Equipo()
+        Dim equipo1 As New Equipo("SM-G950T", New Modelo("S8", New Marca("samsung")))
+        equipo1.Serie = "SM"
+        Console.WriteLine("EQUIPO")
+        Console.WriteLine("Equipo toString: " & equipo1.ToString)
+        Console.WriteLine("Equipo Serie: " & equipo1.Serie)
+        Console.WriteLine("Equipo Fecha Venta: " & equipo1.FechaVenta)
+        Console.WriteLine("E.Modelo tostring:" & equipo1.Modelo.ToString)
+        Console.WriteLine("E.Modelo Nombre: " & equipo1.Modelo.Nombre)
+        Console.WriteLine("E.M.Marca toString: " & equipo1.Modelo.Marca.ToString)
+        Console.WriteLine("E.M.Marca Nombre: " & equipo1.Modelo.Marca.Nombre)
 
+    End Sub
 End Module
--- Telefonia/ConsoleApp2/MainTest.vb
+++ Telefonia/ConsoleApp2/MainTest.vb
@@ -2,5 +2,6 @@
     Sub main()
         Marca()
         Modelo()
+        Equipo()
     End Sub
 End Module
